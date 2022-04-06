103813dd2f0ab4b73aa6ef9ea0ec370cf7dfe8a5
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-04-05 21:45:23 -0500

    Se muestran sugerencias en el buscador y se muestra la info del heroe seleccionado

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '../material/material.module';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
-import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 import { HeroeImagenPipe } from './pipes/heroe-imagen.pipe';
 
 
@@ -25,6 +26,7 @@ import { HeroeImagenPipe } from './pipes/heroe-imagen.pipe';
   ],
   imports: [
     CommonModule,
+    FormsModule, // Habilita el ng model
     HeroesRoutingModule,
     FlexLayoutModule ,
     MaterialModule
--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
@@ -8,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  termino: string = '';
+  heroes: Heroe[] = [];
+  heroeSeleccionado!: Heroe;
+
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
   }
 
+  buscando(): void {
+    this.heroesService.getSugerencias(this.termino).subscribe(heroes =>  this.heroes = heroes);
+  }
+
+  heroeSeleccionadoChanged(e: MatAutocompleteSelectedEvent): void {
+    const heroe: Heroe = e.option.value;
+    this.termino = heroe.superhero;
+    this.heroesService.getHeroeById(heroe.id!).subscribe(heroe => this.heroeSeleccionado = heroe);
+  }
 }
