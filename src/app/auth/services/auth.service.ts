d96328a0221c266c6ffc0aff38348844efaee22b
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-04-17 16:37:56 -0500

    Se muestra el nombre de usuario del user logueado

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { Usuario } from '../interfaces/auth.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = environment.baseUrl;
+  private _usuario: Usuario | undefined;
+
+  get usuario() {
+    return {...this._usuario!};
+  }
+
+  constructor(private http: HttpClient) { }
+
+  login(): Observable<Usuario> {
+    return this.http.get<Usuario>(`${this.baseUrl}/usuarios/1`)
+      .pipe(
+        tap(usuario => { this._usuario = usuario;})
+      );
+  }
+}
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +14,14 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
 
-  constructor(private router: Router) { }
+  get usuario() {
+    return this.authService.usuario;
+  }
 
-  logout(): void {
+  constructor(private router: Router,
+              private authService: AuthService) { }
+
+  logout(): void { 
     this.router.navigate(['/auth']);
   }
 
