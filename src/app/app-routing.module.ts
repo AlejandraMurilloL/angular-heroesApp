8ab8b3d550d26204b7c4ebe3f79d2d5f7d057f20
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-04-17 17:01:51 -0500

    GUARDS - CanLoad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/guards/auth.guard';
 import { Error404Component } from './shared/error404/error404.component';
 
 const routes: Routes = [
@@ -9,7 +10,8 @@ const routes: Routes = [
   },
   {
     path: 'heroes',
-    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule)
+    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule),
+    canLoad: [ AuthGuard ]
   },
   {
     path: '404',
---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanLoad {
+
+  constructor(private authService: AuthService) {}
+
+  // canActivate(
+  //   route: ActivatedRouteSnapshot,
+  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  //   return true;
+  // }
+  
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<UrlTree> | boolean {
+
+    return this.authService.usuario.id ? true : false;
+  }
+}
